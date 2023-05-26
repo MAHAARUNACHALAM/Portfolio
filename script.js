28a0facf55dc6101a0fe7a6aace81e6b0f3f8e94
MAHAARUNACHALAM <smahaarunachalam@gmail.com>
2023-05-26 21:11:50 +0530

    Porfolio contents added

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle the options on hamburger menu click
+document.querySelector('.hamburger-menu').addEventListener('click', function() {
+    document.querySelector('.options').classList.toggle('show');
+  });
+  
